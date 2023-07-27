ea074018577ff43bf9c97c7f1a0f1309f81da7bd
Maxwell Kiana <51776649+kianamax@users.noreply.github.com>
2023-07-27 12:33:49 +0300

    Update process_booking.php

--- process_booking.php
+++ process_booking.php
@@ -2,15 +2,44 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve form data
     $name = $_POST["name"];
-    $tourOption = $_POST["tour_options"];
-    $payment = isset($_POST["payment"]) ? $_POST["payment"] : "-";
+    $tourOption = $_POST["tour_option"];
+    $payment = isset($_POST["payment"]) ? floatval(str_replace('$', '', $_POST["payment"])) : null;
     $email = $_POST["email"];
 
-    // Validation (you can add more validation based on your requirements)
+    
     if (empty($name) || empty($tourOption) || empty($email)) {
         $response = array("status" => "error", "message" => "Please fill in all required fields.");
         header("Content-Type: application/json");
         echo json_encode($response);
+        exit;
+    }
+
+    //database connection
+    $host = "localhost"; 
+    $username = "root";
+    $password = "";
+    $database = "assino";
+
+    // Create a connection to the database
+    $connection = mysqli_connect($host, $username, $password, $database);
+
+    // Check if the connection was successful
+    if (!$connection) {
+        $response = array("status" => "error", "message" => "Database connection failed: " . mysqli_connect_error());
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        exit;
+    }
+
+    // Insert data into the database
+    $insertQuery = "INSERT INTO bookings (name, tour_option, payment, email) VALUES ('$name', '$tourOption', '$payment', '$email')";
+    $result = mysqli_query($connection, $insertQuery);
+
+    if (!$result) {
+        $response = array("status" => "error", "message" => "Failed to insert data into the database: " . mysqli_error($connection));
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        mysqli_close($connection);
         exit;
     }
 
@@ -19,9 +48,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <h2>Booking Invoice</h2>
         <p><strong>Name:</strong> $name</p>
         <p><strong>Tour Option:</strong> $tourOption</p>
-        <p><strong>Payment:</strong> $payment</p>
+        <p><strong>Payment:</strong> $" . number_format($payment, 2) . "</p>
         <p><strong>Email:</strong> $email</p>
     ";
+
+    // Close the database connection
+    mysqli_close($connection);
 
     // Return the invoice as JSON
     $response = array("status" => "success", "invoice" => $invoice);
